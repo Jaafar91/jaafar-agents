53cf8849843ee22806616d174b11ba6e95b0f8ef
mjaafar91 <jaffer12333@hotmail.com>
2026-08-11 19:46:01 +0300

    update db

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, Text
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = os.environ.get(
-    "DATABASE_URL",
-    "postgresql+psycopg2://myuser:mypassword@localhost:5432/car_rental",
-)
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL must be set in the .env file")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
