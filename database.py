25bfca69e3776287176f46dc2b672984fc073631
mjaafar91 <jaffer12333@hotmail.com>
2026-08-11 19:37:21 +0300

    change

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 DATABASE_URL = os.environ.get(
     "DATABASE_URL",
-    "postgresql+psycopg2://myuser:mypassword@localhost:5432/telegram_bot",
+    "postgresql+psycopg2://myuser:mypassword@localhost:5432/car_rental",
 )
 
 engine = create_engine(DATABASE_URL)
